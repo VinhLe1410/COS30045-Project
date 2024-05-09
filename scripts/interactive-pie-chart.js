7f610c6d3bd6d8a9826ae17f5f9c721a036cd362
CodenoobV <128375195+CodenoobV@users.noreply.github.com>
2024-05-09 13:36:20 +1000

    Updating 2 files

--- scripts/interactive-pie-chart.js
+++ scripts/interactive-pie-chart.js
@@ -10,11 +10,36 @@ function init() {
             Year: d.Year,           // Year
             Value: d.Value          // Estimated value
         };
-    }).then(function(data) {        // START OF "THEN"
-        var filtered = data.filter(function(d) {   // START OF FILTER
+    }).then(function(data) {
+        var filtered = data.filter(function(d) {
+            // The only data we need are:
+            //      - From Australia;
+            //      - Total female and male;
+            //      - Both head counts and % of head counts;
             return (d.COU == "AUS" && (d.VAR == "PAGGFEMM" || d.VAR == "PAGGHOMM"))
-        });                         // END OF FILTER
-        console.table(filtered);
-    });                             // END OF "THEN"
+        });
+        // console.table(filtered);
+
+        // filtered_counts: Male physicians and female physicians of Australia (in number)
+        // filtered_percentage: Male physicians and female physicians of Australia (in percentage)
+        var filtered_counts = filtered.filter(function(d) {
+            return d.UNIT == "PERSMYNB";
+        });
+        var filtered_percentage = filtered.filter(function(d) {
+            return d.UNIT == "PHYTOTNB";
+        });
+
+        // oldest_year and latest_year are for the time slider
+        var oldest_year = d3.min(filtered_counts, function(d){
+            return d.Year;
+        });
+        var latest_year = d3.max(filtered_counts, function(d) {
+            return d.Year;
+        });
+
+        // Console log for checking the data
+        console.table(filtered_counts);
+        console.log(oldest_year + " to " + latest_year);
+    });
 }
-window.onload = init; //call function init() whenever the window is onload
+window.onload = init;
