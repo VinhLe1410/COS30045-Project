50cad310ba29a634038bee93bd0ee8ab608c366e
manOwO <maian1611@gmail.com>
2024-05-29 16:33:38 +1000

    add radial stacked barplot, rename files

--- scripts/radial-stacked-barplot.js
+++ scripts/radial-stacked-barplot.js
@@ -1,15 +1,17 @@
 function radialStackedBarplot(dataset) {
 //width and height of svg
-    var w = 500;
-    var h = 500;
-    var innerRadius = 70;
-    var outerRadius = 150;
-    // var outerRadius = Math.min(w, h) / 2;
+    var w = 700;
+    var h = 700;
+    var innerRadius = 125;
+    // var outerRadius = 250;
+    var outerRadius = Math.min(w, h) / 2.2;
 
 //set up the stack
     // list out manually all the keys for stacking to debug
     var stacks = d3.stack() //generate stacks
-                    .keys(["PAGGTOPY", "PAGGTU35", "PAGGT344", "PAGGT344", "PAGGT454", "PAGGT564", "PAGGT65O"]); 
+                    .keys(
+                        ["PAGGTU35", "PAGGT344", "PAGGT344", "PAGGT454", "PAGGT564", "PAGGT65O"]
+                    ); 
                     //specify the categories of interest
 
 //set up the svg canvas
@@ -20,7 +22,7 @@ function radialStackedBarplot(dataset) {
 
 //set up the scales
     var xScale = d3.scaleBand()
-                    .domain(dataset.map(function(d) {return d.Country;}))
+                    .domain(dataset.map(function(d) {return d.COU;}))
                     .range([0, 2 * Math.PI])
                     .paddingInner(0.05);
                     // .align(0);
@@ -33,7 +35,9 @@ function radialStackedBarplot(dataset) {
 
 //color scheme to attach to the arcs group
     var color = d3.scaleOrdinal(d3.schemeCategory10)
-                    .domain(["PAGGTOPY", "PAGGTU35", "PAGGT344", "PAGGT344", "PAGGT454", "PAGGT564", "PAGGT65O"]) ; //d3 native color scheme (no. 10)
+                    .domain(
+                        ["PAGGTU35", "PAGGT344", "PAGGT454", "PAGGT564", "PAGGT65O"]
+                    ) ; //d3 native color scheme (no. 10)
     
 //set up the arcs
     // debugging
@@ -64,8 +68,8 @@ function radialStackedBarplot(dataset) {
             .attr("d", d3.arc()
                         .innerRadius(function(d) { return yScale(d[0]); })
                         .outerRadius(function(d) { return yScale(d[1]); })
-                        .startAngle(function(d) { return xScale(d.data.Country); })
-                        .endAngle(function(d) { return xScale(d.data.Country) + xScale.bandwidth(); })
+                        .startAngle(function(d) { return xScale(d.data.COU); })
+                        .endAngle(function(d) { return xScale(d.data.COU) + xScale.bandwidth(); })
                         .padAngle(0.01)
                         .padRadius(innerRadius)
             );
@@ -78,13 +82,20 @@ function radialStackedBarplot(dataset) {
                     .append("g")
                     .attr("text-anchor", "middle")
                     .attr("transform", function(d) {
-                        return "rotate(" + ((xScale(d.Country) + xScale.bandwidth() / 2) * 180 / Math.PI - 90) + ")translate(" + innerRadius + ",0)"; 
+                        return "rotate(" + ((xScale(d.COU) + xScale.bandwidth() / 2) * 180 / Math.PI - 90) + ")translate(" + innerRadius + ",0)"; 
                     });
+    
+    label.append("line")
+            .attr("x2", -5)
+            .attr("stroke", "#000")
+            .attr("stroke-width", 1);
+    
     label.append("text")
-        .attr("transform", function(d) {
-            return (xScale(d.Country) + xScale.bandwidth() / 2 + Math.PI / 2) % (2 * Math.PI) < Math.PI ? "rotate(90)translate(0,16)" : "rotate(-90)translate(0,-9)"; 
-        })
-        .text(function(d) { return d.Country; });
+            .attr("transform", function(d) {
+                return (xScale(d.COU) + xScale.bandwidth() / 2 + Math.PI / 2) % (2 * Math.PI) < Math.PI ? "rotate(90)translate(0,16)" : "rotate(-90)translate(0,-9)"; 
+            })
+            .text(function(d) { return d.COU; })
+            .attr("font-size", "8px");
 
 //y Axis settings and height of each bars
     var yAxis = groups.append("g")
@@ -98,22 +109,49 @@ function radialStackedBarplot(dataset) {
     //cỉcle playing as line responsible for measuring - ticks
     // debugging
     yTicks.append("circle")
-            .attr("y", function(d) {return -yScale(d);})
-            .attr("dy", "0.35em")
+            .attr("r", yScale)
+            // .attr("dy", "0.35em")
+            .attr("stroke", "#000")
             .attr("fill", "none")
-            .attr("stroke", "#fff")
-            .attr("stroke-width", 5)
-            .text(yScale.tickFormat(5, "s"));
+            .attr("stroke-width", 0.5);
 
     yTicks.append("text")
             .attr("y", function(d) {return -yScale(d);})
             .attr("dy", "0.35em")
-            .text(yScale.tickFormat(5, "s")); //the value at each tick
+            .text(yScale.tickFormat(5, "s"))
+            .attr("font-size", "12px"); //the value at each tick
 
-    // yAxis.append("text")
-    //         .attr("y", function(d) { return -yScale(yScale.ticks(5).pop()); })
-    //         .attr("dy", "-1em")
-    //         .text("Population");
+    yAxis.append("text")
+            .attr("y", function(d) { return -yScale(yScale.ticks(5).pop()); })
+            .attr("dy", "-1.5em")
+            .text("Population")
+            .attr("font-size", "14px");
+
+// legend
+    var legend = groups.append("g")
+                        .selectAll("g")
+                        .data(["PAGGTU35", "PAGGT344", "PAGGT344", "PAGGT454", "PAGGT564", "PAGGT65O"])
+                        .enter()
+                        .append("g")
+                        .attr("transform", function (d, i) {
+                            return "translate(-75," + (i - 2.75) * 20 + ")"; 
+                        });
+
+    legend.append("rect")
+            .attr("width", 18)
+            .attr("height", 18)
+            .attr("fill", function (d, i) { return color(i);});
+    
+    legend.append("text")
+            .attr("x", 24)
+            .attr("y", 9)
+            .text(function (d) { 
+                if (d == "PAGGTU35") return "Under 35 years old";
+                if (d == "PAGGT344") return "35 - 44 years old";
+                if (d == "PAGGT454") return "45 - 54 years old";
+                if (d == "PAGGT564") return "55 - 64 years old";
+                if (d == "PAGGT65O") return "65 - 74 years old";
+            })
 }
 
 function init() {
@@ -140,40 +178,21 @@ function init() {
         });
         console.table(filteredData);
 
-        //long to wide dataset: need to change to the form
-        //debugging
-        // [
-            //  {country: d.Country, PAGGTOPY: d.Value, PAGGTU35: d.Value, PAGGT344: d.Value, PAGGT454: d.Value, ... ncl chia theo VAR},
-        //     {...}, ...
-        // ]
-        // could remove Year as all items have the same year
-        // in progress right now...
-        // var output = d3.nest()
-        //                 .key(function(d) { return d["COU"] })
-        //                 .rollup(function(d) {
-        //                     return d.reduce(function(prev, curr) {
-        //                         prev["COU"] = curr["COU"];
-        //                         prev[curr["VAR"]] = curr["VAR"];
-        //                         return prev;
-        //                     }, {});
-        //                 })
-        //                 .entries(filteredData)
-        //                 .map(function(d) {
-        //                     return d.value;
-        //                 });
+        var longToWideData = d3.rollup(
+            filteredData, 
+            v => Object.fromEntries(v.map(d => [d.VAR, d.Value])),
+            d => d.COU
+        );
 
-        var testDataset = [
-            {Country: 'Australia', PAGGTOPY: 100260, PAGGTU35: 25165, PAGGT344: 28087, PAGGT454: 21667, PAGGT564: 15994, PAGGT65O: 7643 },
-            {Country: 'Austria', PAGGTOPY: 47422, PAGGTU35: 9166, PAGGT344: 12364, PAGGT454: 10614, PAGGT564: 12211, PAGGT65O: 2619 },
-            {Country: 'Belgium', PAGGTOPY: 37089, PAGGTU35: 4539, PAGGT344: 8063, PAGGT454: 8362, PAGGT564: 8696, PAGGT65O: 6131 },
-            {Country: 'Canada', PAGGTOPY: 105504, PAGGTU35: 26409, PAGGT344: 24270, PAGGT454: 20524, PAGGT564: 19300, PAGGT65O: 10897 },
-            {Country: 'Czechia', PAGGTOPY: 43810, PAGGTU35: 9963, PAGGT344: 9343, PAGGT454: 9184, PAGGT564: 8020, PAGGT65O: 5990 },
-            {Country: 'Denmark', PAGGTOPY: 25522, PAGGTU35: 6221, PAGGT344: 6866, PAGGT454: 5163, PAGGT564: 4581, PAGGT65O: 2419 },
-            {Country: 'Finland', PAGGTOPY: 19970, PAGGTU35: 5252, PAGGT344: 5610, PAGGT454: 4371, PAGGT564: 4167, PAGGT65O: 570 },
-            {Country: 'France', PAGGTOPY: 214293, PAGGTU35: 34393, PAGGT344: 41243, PAGGT454: 40744, PAGGT564: 59562, PAGGT65O: 34177 },
-        ];
+        var wideArray = Array.from(longToWideData, ([key, value]) => ({COU: key, ...value}))
 
-        radialStackedBarplot(testDataset);
+        console.log(wideArray);
+
+        var processedData = wideArray.filter(function(d) {
+            return typeof d.PAGGTU35 != 'undefined' && typeof d.PAGGT344 != 'undefined' && typeof d.PAGGT454 != 'undefined' && typeof d.PAGGT564 != 'undefined' && typeof d.PAGGT65O != 'undefined';
+        });
+
+        radialStackedBarplot(processedData);
 
     });
 }
